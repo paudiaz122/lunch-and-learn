15632c6445ef10e27a268fcb60aabc60bc8d3268
Santiago Botero Ruiz <santiago.botero@devsavant.ai>
2021-07-08 12:45:42 -0500

    Se agrega factorial de 3

--- factorial.js
+++ factorial.js
@@ -5,7 +5,8 @@ const factorial = (n) => {
     return factorial(n - 1) * n;
 }
 
-console.log('Factorial 4: ', factorial(5));
+console.log('Factorial 3: ', factorial(3));
+console.log('Factorial 4: ', factorial(4));
 console.log('Factorial 5: ', factorial(5));
 console.log('Factorial 6: ', factorial(6));
 console.log('Factorial 7: ', factorial(7));
